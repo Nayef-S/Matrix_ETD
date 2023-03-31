cce428e07ae05a02e0fdab6839f858e5fd29cd5c
Nayef Shkeir <48997096+Nayef-S@users.noreply.github.com>
2023-03-31 18:52:50 +0100

    lower dt

--- Toy_examples/lyapunov_example.py
+++ Toy_examples/lyapunov_example.py
@@ -11,24 +11,24 @@ plt.rcParams.update({'font.size': 14})
 #initialisation
 rng = np.random.default_rng(1)
 
-L = np.array([[-1,1],[1,-2]])
+L = -1 * np.asarray([[2,-1],[1,2]])
 R = L.T
 N = np.asarray([[7,2],[2,7]])
 comm_NR = N@R - R@N 
 
-dt_array = [0.00005, 0.0001, 0.0005, 0.001 ,0.005 , 0.01, 0.05, 0.1, 0.5, 1]
+dt_array = [1e-5, 0.00005, 0.0001, 0.0005, 0.001 ,0.005 , 0.01, 0.05, 0.1]
 
 error_array_1 = np.zeros(len(dt_array))
 error_array_2 = np.zeros(len(dt_array))
 time_array = np.zeros(len(dt_array))
 
-A_true = linalg.solve_sylvester(L,R,-1.0 * N)
+A_true = linalg.solve_sylvester(L,R,-N)
 
 for i, dt in tqdm(enumerate(dt_array)):
     A_0_1 = rng.standard_normal(size=L.shape)
     A_0_2 = rng.standard_normal(size=L.shape)
 
-    t_final = 100
+    t_final = 10
     n_steps = round(t_final/dt)
 
     m1_L_1, m1_R_1, m2_1 = ETD1(L, R, dt)
@@ -45,7 +45,7 @@ for i, dt in tqdm(enumerate(dt_array)):
 plt.plot(time_array, error_array_1, 'b-', label=r'ETD1 error ')
 plt.plot(time_array, error_array_2, 'r-', label=r'ETD2 error')
 plt.plot(time_array, time_array, 'b--', label=r'$ \mathcal{O}(\Delta t)$')
-plt.plot(time_array, time_array**2, 'r--', label=r'$ \mathcal{O}(\Delta t^2)$')
+plt.plot(time_array, 2* time_array**2, 'r--', label=r'$ \mathcal{O}(\Delta t^2)$')
 plt.yscale('log')
 plt.xscale('log')
 plt.ylabel(r'$|| C_{\infty} - C^{(n)}||_{2}$')
